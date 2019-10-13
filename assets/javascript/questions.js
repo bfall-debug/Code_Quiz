54c5d5bfd271a150f4a33afd15c96317d2f0048b
BFall-Debug <bfall92@gmail.com>
2019-10-13 17:28:53 -0400

    list of questions added

---
+++ assets/javascript/questions.js
@@ -0,0 +1,62 @@
+var questions = [
+    {
+        // Question 1
+        title: "What is the element used – and hidden – in code that explains things and makes the content more readable?",
+        choices: ["Comments", "Quotations", "Comparisons", "Notes"],
+        answer: "Comments"
+    },
+    {
+        // Question 2
+        title: "What can loops offer JavaScript code as a whole?",
+        choices: ["Improved Performance", "Added Plug-ins", "Cleaner Syntax", "Cross-Platform Support"],
+        answer: "Improved Performance"
+    },
+    {
+        // Question 3
+        title: "What is the name of the object that allows you to perform mathematical tasks with the interpreter?",
+        choices: ["Math", "Count", "Solve", "Number"],
+        answer: "Math"
+    },
+    {
+        // Question 4
+        title: "In JavaScript, what element is used to store and manipulate text, usually in multiples?",
+        choices: ["Arrays", "Strings", "Variables", "Recorders"],
+        answer: "Strings"
+    },
+    {
+        // Question 5
+        title: "In JavaScript, what element is used to store multiple values in a single variable?",
+        choices: ["Arrays", "Strings", "Variables", "Functions"],
+        answer: "Arrays"
+    }, 
+    {
+        // Question 6
+        title: "What elements are used to test for TRUE or False values stored in variables?",
+        choices: ["RegExp or Regular Expressions", "Trigger Readerss", "Conditional Statements", "Comparison and Logical Operators"],
+        answer: "Comparison and Logical Operators"
+    },
+    {
+        // Question 7
+        title: "What is a JavaScript element that represents either TRUE or FALSE values?",
+        choices: ["Condition", "Event", "RegExp", "Booleann"],
+        answer: "Booleann"
+    },
+    {
+        // Question 8
+        title: "What is considered to be the most popular programming language in the world?",
+        choices: ["JavaScript", "Swift", "Ruby", "HTML"],
+        answer: "JavaScript"
+    },
+    {
+        // Question 9
+        title: "JavaScript does NOT have this function built-in, which is commonly used to print or display data in other languages.",
+        choices: ["Show", "Display", "Speak", "Output"],
+        answer: "Output"
+    },
+    {
+        // Question 10
+        title: "In JavaScript, what is used in conjunction with HTML to “react” to certain elements?",
+        choices: ["Condition", "Boolean", "RegExp", "Events"],
+        answer: "Events"
+    }
+];
